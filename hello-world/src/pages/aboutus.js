7eb10cd8774da1e28f042bb25e34fb78750cf42c
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-01-28 04:01:57 +0530

    Update aboutus.js

--- hello-world/src/pages/aboutus.js
+++ hello-world/src/pages/aboutus.js
@@ -3,6 +3,7 @@ import Header from "../components/header"
 export default () => (
   <div style={{ color: `teal` }}>
     <Header headerText="About Gatsby"/> 
+  <Header headerText="It's pretty cool" /> 
     <p>Such wow. Very React.</p>
   </div>
 )

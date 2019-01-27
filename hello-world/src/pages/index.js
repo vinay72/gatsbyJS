dac98e1be6b3714776075bfc40fd00e406f81bff
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-01-28 02:19:43 +0530

    Update index.js

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -4,5 +4,6 @@ export default () => (
 <div style={{ color: `purple` }}>    
   <h1>Hello Gatsby!</h1>   
    <p>What a world.</p>  
+  <img src="https://source.unsplash.com/random/400x200" alt="" />
    </div>
 )  

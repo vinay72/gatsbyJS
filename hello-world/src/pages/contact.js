9f0dfaa000a923a1a1a192ca603ccd064badc2d5
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-05 03:29:51 +0530

    Update contact.js

--- hello-world/src/pages/contact.js
+++ hello-world/src/pages/contact.js
@@ -6,6 +6,10 @@ export default () => (
   <div style={{ color: `teal` }}>
     <Link to="/">Home</Link>
     <Header headerText="Contact" />
+    <h1>I would love to talk! Email me at the address below</h1>
     <p>Send us a message!</p>
+  <p>
+      <a href="mailto:vinaysagar971995@gmail.com">vinaysagar971995.com</a>
+    </p>
   </div>
 )

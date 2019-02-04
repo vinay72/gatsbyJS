25a45c5ed71ab43c3794fa710cc6811053c06ab6
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-05 04:07:37 +0530

    Update contact.js

--- hello-world/src/pages/contact.js
+++ hello-world/src/pages/contact.js
@@ -1,15 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout"
 
 export default () => (
   <div style={{ color: `teal` }}>
     <Link to="/">Home</Link>
     <Header headerText="Contact" />
+    <Layout>
     <h1>I would love to talk! Email me at the address below</h1>
     <p>Send us a message!</p>
-  <p>
+    <p>
       <a href="mailto:vinaysagar971995@gmail.com">vinaysagar971995.com</a>
     </p>
+    </Layout>
   </div>
 )

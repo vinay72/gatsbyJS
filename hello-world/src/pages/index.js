5388b0015e5596b5b12dac18f7ee09aaaae2887e
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-05 02:25:05 +0530

    Update index.js

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -2,13 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
 export default () => (
-  <div style={{ color: `purple` }}>    
-  <Link to="/contact/">Contact</Link>  
-  <Header headerText="Hello Gatsby!" />   
-  <h1>Hi! I'm building a demo Gatsby site as part of a tutorial!</h1>
-   <p>What a world.
-      What I like to do? Lots of course but definitely enjoy building
+  <div>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
       websites.
     </p>
-   </div>
+  </div>
 )

d8b8f7de40c740653b81d021607a6d04fde020b4
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-05 04:02:09 +0530

    Update index.js

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -5,8 +5,12 @@ export default () => (
    <div style={{ margin: `3rem auto`, maxWidth: 600, color: `purple` }}>    
   <Link to="/contact/">Contact</Link> 
   <Header headerText="Hello Gatsby!" />  
-   h1>Hi! I am building a demo Gatsby site as part of a tutorial!</h1>
-   <p>What a world. What  I like  ? Lots of course but definitely enjoy building
-      websites.</p>  
+   <Layout>
+     <h1>Hi! I am building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
+    </Layout>
   </div>
 )

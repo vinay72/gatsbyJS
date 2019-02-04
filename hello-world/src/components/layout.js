3cc81bce7abe7c37bfa3d917087b259df0c10350
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-05 04:31:45 +0530

    Update layout.js

--- hello-world/src/components/layout.js
+++ hello-world/src/components/layout.js
@@ -1,7 +1,27 @@
 import React from "react"
+import { Link } from "gatsby"
+
+
+const ListLink = props => (  
+	<li style={{ display: `inline-block`, marginRight: `1rem` }}>    
+	<Link to={props.to}>{props.children}</Link>  
+	</li>
+)
 
 export default ({ children }) => (
-  <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
+  <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `1.25rem 1rem` }}>
+
+  <header style={{ marginBottom: `1.5rem` }}>      
+  <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>       
+   <h3 style={{ display: `inline` }}>MySweetSite</h3>      
+   </Link>      
+   <ul style={{ listStyle: `none`, float: `right` }}>        
+   <ListLink to="/">Home</ListLink>        
+   <ListLink to="/aboutus/">About</ListLink>        
+   <ListLink to="/contact/">Contact</ListLink>
+   <ListLink to="/about-css-modules/">CSS Modules</ListLink>      
+   </ul>    
+   </header>
     {children}
   </div>
 )

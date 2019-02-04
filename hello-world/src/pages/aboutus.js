9f3a600d92f1f3a7c0e503270262e54197a368ff
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-05 03:27:57 +0530

    Update aboutus.js

--- hello-world/src/pages/aboutus.js
+++ hello-world/src/pages/aboutus.js
@@ -4,6 +4,8 @@ export default () => (
   <div style={{ color: `teal` }}>
     <Header headerText="About Gatsby"/> 
   <Header headerText="It's pretty cool" /> 
+  <h1>About me</h1>
+      <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
     <p>Such wow. Very React.</p>
   </div>
 )

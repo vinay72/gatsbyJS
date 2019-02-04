aa174f994dbd23cec18c98cfd5b91f5e6a68c9b4
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-05 02:29:02 +0530

    Update index.js

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -5,7 +5,7 @@ export default () => (
    <div style={{ color: `purple` }}>    
   <Link to="/contact/">Contact</Link> 
   <Header headerText="Hello Gatsby!" />  
-   h1>Hi! I'm building a demo Gatsby site as part of a tutorial!</h1>
+   h1>Hi! I am building a demo Gatsby site as part of a tutorial!</h1>
    <p>What a world. What  I like  ? Lots of course but definitely enjoy building
       websites.</p>  
   </div>

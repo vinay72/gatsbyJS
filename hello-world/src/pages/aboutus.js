3b7f6dadc2590c3f4ea5a5500aff1f9fc999e66b
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-05 04:07:08 +0530

    Update aboutus.js

--- hello-world/src/pages/aboutus.js
+++ hello-world/src/pages/aboutus.js
@@ -1,11 +1,15 @@
 import React from "react"
 import Header from "../components/header"
+import Layout from "../components/layout"
+
 export default () => (
   <div style={{ color: `teal` }}>
     <Header headerText="About Gatsby"/> 
-  <Header headerText="It's pretty cool" /> 
-  <h1>About me</h1>
+     <Header headerText="It's pretty cool" /> 
+     <Layout>
+     <h1>About me</h1>
       <p>I’m good enough, I’m smart enough, and gosh darn it, people like me!</p>
     <p>Such wow. Very React.</p>
+    </Layout>
   </div>
 )

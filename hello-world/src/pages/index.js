bad9f152e2b21fa6737725e6f00c85e864868010
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-05 04:06:31 +0530

    Update index.js

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -1,16 +1,22 @@
 import React from "react"
 import { Link } from "gatsby"
 import Header from "../components/header"
+import Layout from "../components/layout"
+
 export default () => (
-   <div style={{ margin: `3rem auto`, maxWidth: 600, color: `purple` }}>    
+	
+  <div style={{ margin: `3rem auto`, maxWidth: 600, color: `purple` }}>    
   <Link to="/contact/">Contact</Link> 
-  <Header headerText="Hello Gatsby!" />  
-   <Layout>
+  <Header headerText="Hello Gatsby!" />   
+  
+        <Layout>
      <h1>Hi! I am building a fake Gatsby site as part of a tutorial!</h1>
     <p>
       What do I like to do? Lots of course but definitely enjoy building
       websites.
     </p>
     </Layout>
-  </div>
+    
+   
+   </div>
 )

77b06319da4bbed044fcf92a6720af42e1a93961
Vinay Sagar Sharma <vinaysagar971995@gmail.com>
2019-02-05 02:28:14 +0530

    Update index.js

--- hello-world/src/pages/index.js
+++ hello-world/src/pages/index.js
@@ -4,7 +4,8 @@ import Header from "../components/header"
 export default () => (
    <div style={{ color: `purple` }}>    
   <Link to="/contact/">Contact</Link> 
-  <Header headerText="Hello Gatsby!" />   
+  <Header headerText="Hello Gatsby!" />  
+   h1>Hi! I'm building a demo Gatsby site as part of a tutorial!</h1>
    <p>What a world. What  I like  ? Lots of course but definitely enjoy building
       websites.</p>  
   </div>
